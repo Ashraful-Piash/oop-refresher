b926ee4ec7e2ae9486de73a24061aca716361b95
Ashraful-Piash <piashrafulhaque@gmail.com>
2024-03-16 03:31:39 +0600

    updated

---
+++ src/piashraful/atm/ExampleOfInheritance.java
@@ -0,0 +1,36 @@
+package piashraful.atm;
+
+class Vehicle{
+    String name;
+    int price;
+    String colour;
+
+    public void displayInformation(){
+        System.out.println(name);
+        System.out.println(price);
+        System.out.println(colour);
+        
+    }
+}
+
+class Car extends Vehicle{
+    String brand;
+}
+
+
+
+
+
+
+
+public class ExampleOfInheritance {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.name = "My Car";
+        car.brand = "Ferrari";
+        car.colour = "RED";
+        car.price = 100000;
+       car.displayInformation();
+
+    }
+}
